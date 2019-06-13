43716717269b9afec2056814bf34bd17985e8bc6
Will Smith <william.smith@stfc.ac.uk>
2019-06-13 15:14:06 +0100

    setup in standalone mode, uses PS0 to replicate trigger of softseq0

--- st.ral-evr-01.cmd
+++ st.ral-evr-01.cmd
@@ -17,19 +17,19 @@ dbLoadRecords("$(MRF_HW_DB)","EVR=$(EVR),SYS=$(SYS),D=$(DEVICE),FEVT=$(ESSEvtClo
 
 
 ############# -------- Detector Readout Interface ----------------- ##################
-#epicsEnvSet("DETINT_CMD_TOP","/epics7/iocs/cmds/ral-evr-01") 
-#epicsEnvSet("STREAM_PROTOCOL_PATH","/epics7/base-7.0.2/require/3.1.0/siteApps/dmsc_detector_interface/master/db")
+epicsEnvSet("DETINT_CMD_TOP","/epics7/iocs/cmds/ral-evr-01") 
+epicsEnvSet("STREAM_PROTOCOL_PATH","/epics7/base-7.0.2/require/3.1.0/siteApps/dmsc_detector_interface/master/db")
 
 epicsEnvSet("DET_CLK_RST_EVT", "15")
 epicsEnvSet("DET_RST_EVT", "15")
 epicsEnvSet("SYNC_EVNT_LETTER", "EvtF")
 epicsEnvSet("SYNC_TRIG_EVT", "16")
-epicsEnvSet("NANO_DELTA", "1000000000")
+epicsEnvSet("NANO_DELTA", "4425950")
 
 # Load the detector interface module
 
-#system "/usr/bin/python $(DETINT_CMD_TOP)/generate_cmd_file.py --path $(DETINT_CMD_TOP) --serial_ports ttyUSB2 ttyUSB3"
-#iocshLoad("$(DETINT_CMD_TOP)/detint.cmd", "DEV1=RO1, DEV2=RO2, COM1=COM1, COM2=COM2, SYS=$(SYS), SYNC_EVNT=$(DET_RST_EVT), SYNC_EVNT_LETTER=$(SYNC_EVNT_LETTER), N_SEC_TICKS=1000000000 ")
+system "/usr/bin/python $(DETINT_CMD_TOP)/generate_cmd_file.py --path $(DETINT_CMD_TOP) --serial_ports ttyUSB2 ttyUSB3"
+iocshLoad("$(DETINT_CMD_TOP)/detint.cmd", "DEV1=RO1, DEV2=RO2, COM1=COM1, COM2=COM2, SYS=$(SYS), SYNC_EVNT=$(DET_RST_EVT), SYNC_EVNT_LETTER=$(SYNC_EVNT_LETTER), N_SEC_TICKS=1000000000 ")
 
 
 
@@ -45,102 +45,45 @@ dbpf $(SYS)-$(DEVICE):Src-Clk-SP "Internal"
 # is disabled to avoid occasionally corrupting timestamps.
 dbpf $(SYS)-$(DEVICE):DC-Tgt-SP 70
 
-# Set up the prescaler that will trigger teh sequencer event at 14Hz
-dbpf $(SYS)-$(DEVICE):PS0-Div-SP 6289464
+# Set up the prescaler that will trigger the sequencer (representing evnt 125) at 1Hz
+dbpf $(SYS)-$(DEVICE):PS0-Div-SP 88051900
 
-dbpf $(SYS)-$(DEVICE):SoftSeq0-RunMode-Sel "Normal"
+dbpf $(SYS)-$(DEVICE):SoftSeq0-RunMode-Sel "Single"
 dbpf $(SYS)-$(DEVICE):SoftSeq0-TrigSrc-2-Sel "Prescaler 0"
-dbpf $(SYS)-$(DEVICE):SoftSeq0-TsResolution-Sel "uSec"
+dbpf $(SYS)-$(DEVICE):SoftSeq0-TsResolution-Sel 0
 dbpf $(SYS)-$(DEVICE):SoftSeq0-Load-Cmd 1
-dbpf $(SYS)-$(DEVICE):SoftSeq0-Enable-Cmd 1
-
-
-
-
-
-
-
-
-
+dbpf $(SYS)-$(DEVICE):SoftSeq0-Enable-Cmd 0
 
 
 # Connect prescaler reset to event $(DET_CLK_RST_EVT)
-#dbpf $(SYS)-$(DEVICE):Evt-ResetPS-SP $(DET_CLK_RST_EVT)
+dbpf $(SYS)-$(DEVICE):Evt-ResetPS-SP $(DET_CLK_RST_EVT)
 
 
 # Map pulser 9 to event code SYNC_TRIG_EVT
-#dbpf $(SYS)-$(DEVICE):DlyGen9-Evt-Trig0-SP $(SYNC_TRIG_EVT)
-#dbpf $(SYS)-$(DEVICE):DlyGen9-Width-SP 10
+dbpf $(SYS)-$(DEVICE):DlyGen9-Evt-Trig0-SP $(SYNC_TRIG_EVT)
+dbpf $(SYS)-$(DEVICE):DlyGen9-Width-SP 10
 
-# Set up Prescaler 0 
-#dbpf $(SYS)-$(DEVICE):PS0-Div-SP 2
+# Set up Prescaler 1 
+dbpf $(SYS)-$(DEVICE):PS1-Div-SP 2
 
+# Connect FP06 to PS1
+dbpf $(SYS)-$(DEVICE):OutFPUV06-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV06-Src-SP 41 
 
+# Connect FP07 to Pulser 9
+dbpf $(SYS)-$(DEVICE):OutFPUV07-Ena-SP 1
+dbpf $(SYS)-$(DEVICE):OutFPUV07-Src-SP 9 
 
-
-# Connect FP10 to PS0
-dbpf $(SYS)-$(DEVICE):OutFPUV10-Ena-SP 1
-dbpf $(SYS)-$(DEVICE):OutFPUV10-Src-SP 40 
-
-# Connect FP11 to Pulser 9
-#dbpf $(SYS)-$(DEVICE):OutFPUV11-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV11-Src-SP 9 
-
-
-# Connect FP12 to PS0
-#dbpf $(SYS)-$(DEVICE):OutFPUV12-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV12-Src-SP 40 
-
-# Connect FP09 to PS0
-#dbpf $(SYS)-$(DEVICE):OutFPUV09-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV09-Src-SP 40 
-
-# Connect FP13 to Pulser 9
-#dbpf $(SYS)-$(DEVICE):OutFPUV13-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV13-Src-SP 9 
-
-
-
-
-# Map pulser 7 to event code 125
-#dbpf $(SYS)-$(DEVICE):DlyGen7-Evt-Trig0-SP 125
-#dbpf $(SYS)-$(DEVICE):DlyGen7-Width-SP 10
-
-
-# Connect FP2 to Pulser 9
-#dbpf $(SYS)-$(DEVICE):OutFPUV02-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV02-Src-SP 9 
-
-# Connect FP3 to Pulser 9
-#dbpf $(SYS)-$(DEVICE):OutFPUV03-Ena-SP 1
-#dbpf $(SYS)-$(DEVICE):OutFPUV03-Src-SP 9 
 
 ######## load the sync sequence ######
 
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-Disable-Cmd 1
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-Unload-Cmd 1
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-Load-Cmd 1
-
-#Use ticks
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-TsResolution-Sel  "0"
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-Commit-Cmd 1
-
-#connect the sequence to software trigger
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-TrigSrc-Scale-Sel "Software"
-#connect the sequence to software trigger
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-TrigSrc-Pulse-Sel "Pulser 7"
-
-#dbpf $(SYS)-$(DEVICE):SoftSeq0-RunMode-Sel "Single"
-
 #add sequence events and corresponding tick lists
-#system "/bin/bash /epics7/iocs/cmds/ral-evr-01/evr_seq_sync.sh"
-
-
+system "/bin/bash /epics7/iocs/cmds/ral-evr-01/evr_seq_sync.sh"
  
-#perform sync one next event 125
+#perform sync one next event PS0 Overflow
 #dbpf $(SYS)-$(DEVICE):SoftSeq0-Enable-Cmd 1
 
 
-#dbpf $(SYS)-$(DEVICE):syncTrigEvt-SP $(SYNC_TRIG_EVT)
-#dbpf $(SYS)-$(DEVICE):FracNsecDelta-SP 88052500 
+dbpf $(SYS)-$(DEVICE):syncTrigEvt-SP $(SYNC_TRIG_EVT)
+dbpf $(SYS)-$(DEVICE):FracNsecDelta-SP 44025950 
 									  
